104316c1480cf7a90a017bce244a6828367d451b
rzwei <11663545+not-rzwei@users.noreply.github.com>
2025-09-20 14:26:37 +0900

    ci: use ppocr_v5

--- ci/configure.py
+++ ci/configure.py
@@ -7,14 +7,9 @@ assets_dir = Path(__file__).parent.parent / "assets"
 
 def configure_ocr_model():
     shutil.copytree(
-        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "en_us",
+        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v5" / "zh_cn",
         assets_dir / "resource" / "base" / "model" / "ocr",
         dirs_exist_ok=True,
-    )
-
-    shutil.copy2(
-        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v3" / "en_us" / "det.onnx",
-        assets_dir / "resource" / "base" / "model" / "ocr" / "det.onnx",
     )
 
 
